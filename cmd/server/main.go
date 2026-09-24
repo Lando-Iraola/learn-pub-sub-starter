8b50e576e0bec2bcedb1ac31182c5d10691d812e
Lando <lando_vargas@outlook.com>
2026-09-23 21:47:21 -0300

    CH2: L3

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log/slog"
+	"os"
+	"os/signal"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
 
 func main() {
 	fmt.Println("Starting Peril server...")
+
+	conString := "amqp://guest:guest@localhost:5672/"
+	conn, err := amqp.Dial(conString)
+	if err != nil {
+		slog.Error("Failed to connect to rabbitmq", err)
+	}
+	defer conn.Close()
+
+	fmt.Println("Connected to rabbitmq")
+
+	signalChan := make(chan os.Signal, 1)
+	signal.Notify(signalChan, os.Interrupt)
+
+	interrupted := <-signalChan
+	fmt.Printf("%v Signal, shutting down\n", interrupted)
+	os.Exit(0)
 }
