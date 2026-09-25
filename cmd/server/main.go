f76b5068b58a77a7bb1e08ed6464e55dfaf7c36b
Lando <lando_vargas@outlook.com>
2026-09-25 00:58:06 -0300

    CH3, L5

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/signal"
 
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/gamelogic"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
 	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 	amqp "github.com/rabbitmq/amqp091-go"
@@ -30,6 +31,30 @@ func main() {
 	pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
 	fmt.Println("Peril game server connected to RabbitMQ!")
 
+	gamelogic.PrintServerHelp()
+	for {
+		inputs := gamelogic.GetInput()
+		if len(inputs) == 0 {
+			continue
+		}
+
+		firstWord := inputs[0]
+
+		switch firstWord {
+		case "pause":
+			log.Println("Pause message")
+			pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true})
+		case "resume":
+			log.Println("Resume message")
+			pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: false})
+		case "quit":
+			log.Println("Quit received, quitting")
+			break
+		default:
+			log.Println("Unknown command received")
+		}
+
+	}
 	signalChan := make(chan os.Signal, 1)
 	signal.Notify(signalChan, os.Interrupt)
 
